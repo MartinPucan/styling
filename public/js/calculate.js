5ea56802d4c46b89bc6ae10d5ba7e44b7545620c
MartinPucan <pucan.m@seznam.cz>
2020-08-27 23:35:39 +0200

    fix calculator

--- public/js/calculate.js
+++ public/js/calculate.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
 		const cislo1 = $("#cislo1").val();
 		const cislo2 = $("#cislo2").val();
 		const vysledek = parseInt(cislo1) + parseInt(cislo2);
-		alert(vysledek);
+
+		if (!isNaN(vysledek)) {
+			return alert(vysledek);
+		}
 	});
 });
