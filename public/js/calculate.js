91c431cf338ec445b81703c43ff134f43a766586
MartinPucan <pucan.m@seznam.cz>
2020-08-27 16:09:58 +0200

    need finish math random

---
+++ public/js/calculate.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$("#tlacitko").mouseenter(function() {
+		const cislo1 = $("#cislo1").val();
+		const cislo2 = $("#cislo2").val();
+		const vysledek = parseInt(cislo1) + parseInt(cislo2);
+		alert(vysledek);
+	});
+});
