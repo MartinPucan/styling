07826818b252f0848c627addb5d3f24e5e88b896
MartinPucan <pucan.m@seznam.cz>
2020-09-01 16:57:30 +0200

    start js preview

--- public/js/slider.js
+++ public/js/slider.js
@@ -70,9 +70,9 @@
 	};
 
 	$.fn.easySlider = function(method) {
-		if ( methods[method] ) {
+		if (methods[method]) {
 			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		} else if ( typeof method == 'object' || !method ) {
+		} else if (typeof method === 'object' || !method) {
 			return methods.init.apply(this, arguments);
 		} else {
 			$.error('Metoda ' + method + ' neexistuje v plug-inu jQuery.easySlider');
@@ -82,7 +82,7 @@
 
 	$.fn.easySlider.defaults = {
 		active: 1,
-		speed: 500
+		speed: 11100
 	};
 
 })( jQuery );
