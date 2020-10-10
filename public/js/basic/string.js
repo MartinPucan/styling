9492c4f9fd1a8e2579bc3cde47c085443e22e457
MartinPucan <pucan.m@seznam.cz>
2020-10-10 23:57:48 +0200

    complete strings

--- public/js/basic/string.js
+++ public/js/basic/string.js
@@ -4,7 +4,13 @@ const lastName = 'Doe';
 console.log(firstName.toLowerCase());
 console.log(firstName.toUpperCase());
 
-const completeName = firstName + lastName;
+const completeName = firstName + ' ' + lastName;
 
 console.log(completeName);
+
+const modifiedName = `<h2>${completeName}</h2>`;
+
+document.querySelector('span').innerHTML = modifiedName;
+
+
 
