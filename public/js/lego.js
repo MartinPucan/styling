16f991c10a182aed18d97a5d3c5343c4590cab87
MartinPucan <pucan.m@seznam.cz>
2021-01-28 15:14:40 +0100

    add js file to lego and tried new features from es12

--- public/js/lego.js
+++ public/js/lego.js
@@ -1,13 +1,19 @@
-const div = document.querySelector('root');
+const div = document.getElementById('root');
+const stringSelector = document.getElementById('string');
 
+const sentence = "ahoj, ahoj, cau";
+const newSentence = sentence.replaceAll("ahoj", "cus");
+console.log(newSentence);
 const team = [
-  'Burns',
-  'Hertl',
-  'Pavelski',
-  'Thorton',
-  'Couture'
+  'Adam',
+  'Vlada',
+  'Mama'
 ];
 
-const formattedTeam = new Intl.ListFormat('en-GB', {style: 'long', type: 'conjunction'}).format(team);
+const formattedTeam = new Intl.ListFormat('cs-CS', {style: 'long', type: 'disjunction'}).format(team);
+const formattedTeamCZ = new Intl.ListFormat('en-US', {style: 'long', type: 'conjunction'}).format(team);
 
 div.innerHTML = `<div>${formattedTeam}</div>`;
+div.innerHTML += `<div>${formattedTeamCZ}</div>`;
+
+console.log("it works.");
